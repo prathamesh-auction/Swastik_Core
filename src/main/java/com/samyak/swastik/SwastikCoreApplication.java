e871b8e4367090f2d1108a4ba8ea878e6140dbc5
prathamesh-auction <prathamesh.esamyak@gmail.com>
2024-06-06 11:28:35 +0530

    initial commit

---
+++ src/main/java/com/samyak/swastik/SwastikCoreApplication.java
@@ -0,0 +1,13 @@
+package com.samyak.swastik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwastikCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwastikCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samyak/swastik/SwastikCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samyak.swastik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwastikCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
